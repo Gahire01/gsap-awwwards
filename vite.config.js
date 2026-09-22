021fe99492644048ab2f0f8ac4ce8097bc3b547d
Gahire01 <gahiredev01@gmail.com>
2026-09-22 12:11:27 +0200

    fix: use dist output for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Spylt-awward-clone/" : "/",
-
   server: {
     port: 5175,
-    strictPort: false, // If 5173 is busy, Vite automatically tries the next free port
+    strictPort: false,
   },
 
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
 
   plugins: [react(), tailwindcss()],
